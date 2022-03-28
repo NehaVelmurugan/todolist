4380f954f457be30a6acb027731671950633b148
KubeerVignesh <vigneshkubeer@gmail.com>
2022-03-28 10:13:48 +0530

    oneWayBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product/product.component';
-import { BooksComponent } from './Books/books/books.component';
+import { OneWayBindingComponent } from './one-way-binding/one-way-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    BooksComponent
+    OneWayBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/one-way-binding/one-way-binding.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-one-way-binding',
+  templateUrl: './one-way-binding.component.html',
+  styleUrls: ['./one-way-binding.component.css'],
+})
+export class OneWayBindingComponent implements OnInit {
+  frd = { id: 1, name: 'John', salary: 12000 };
+  status = false;
+  status1 = false;
+  className = 'propertyBinding';
+  className1 = 'attributeBinding';
+
+
+  @Output() public childName = new EventEmitter();
+
+  childData(){
+    this.childName.emit("This is My App");
+  }
+
+
+  constructor() {
+    setTimeout(() => {
+      this.status = false;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+}
